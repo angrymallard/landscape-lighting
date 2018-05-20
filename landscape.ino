49ebeaf4f95e619ecd10f79bd02d793e7f3e79aa
angrymallard <bryan.cockfield@gmail.com>
2018-05-19 20:05:33 -0400

    Update landscape.ino

--- landscape.ino
+++ landscape.ino
@@ -4,3 +4,4 @@
 //once the lights are on, if the microcontroller loses main power, it goes into sleep mode while being powered by a 1F capacitor
 //if it goes into sleep mode, it turns the lights off and will not turn them back on until the next day.
 
+a
